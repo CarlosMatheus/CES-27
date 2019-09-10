c630676ed3d40a7e6871ad92b4b2676d267371a5
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-09-10 16:04:36 -0300

    Adjust shared resource

--- lab02/SharedResource.go
+++ lab02/SharedResource.go
@@ -3,11 +3,9 @@ package main
 import (
 	"bufio"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net"
 	"os"
-	"strconv"
 	"time"
 )
 
@@ -19,25 +17,16 @@ type ClockStruct struct {
 }
 
 var err string
-var myPort string
-var nServers int
-var CliConn []*net.UDPConn
 var ServerConn *net.UDPConn
 var ch chan string
-var myId string
-var logicalClock ClockStruct
 
-/* Simple function to verify error */
 func CheckError(err error) {
+	/*
+		Simple function to verify error
+	*/
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(0)
-	}
-}
-
-func PrintError(err error) {
-	if err != nil {
-		fmt.Println("Error: ", err)
 	}
 }
 
@@ -51,70 +40,18 @@ func doServerJob() {
 	err = json.Unmarshal(buf[:n], &logicalClockReceived)
 	CheckError(err)
 
-	if logicalClockReceived.Clock > logicalClock.Clock {
-		logicalClock.Clock = logicalClockReceived.Clock
-	}
-
-	logicalClock.Clock++
-
-	fmt.Println("Current logical Clock:", logicalClock.Clock)
+	fmt.Println("Received logical Clock:", logicalClockReceived.Clock)
 	fmt.Println("Received", logicalClockReceived.Message)
 
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-}
-
-func doClientJob(otherProcess int) {
-	jsonRequestByte, err := json.Marshal(logicalClock)
-	CheckError(err)
-
-	buf := jsonRequestByte
-	_, err = CliConn[otherProcess].Write(buf)
-	if err != nil {
-		fmt.Println(jsonRequestByte, err)
-	}
-	time.Sleep(time.Second * 1)
-}
-
-func getMyPortNumber(portArg string, myId string) string {
-	portNum, err := strconv.Atoi(portArg[1:])
-	CheckError(err)
-
-	idNum, err := strconv.Atoi(myId)
-	CheckError(err)
-
-	num := portNum - idNum + 1
-
-	newPortStr := strconv.Itoa(num)
-
-	return ":" + newPortStr
 }
 
 func initConnections() error {
+	/*
+		init shared resources server
+	 */
 	ch  = make(chan string)
-	nonOtherServers := 2
-	nServers = len(os.Args) - nonOtherServers
-	myPort = ":10001"
-	CliConn = make([]*net.UDPConn, nServers)
-	logicalClock = ClockStruct{Id: 0, Clock: 0, Message: "", Request: false}
 
-	// Init client
-	for otherProcess := 0; otherProcess < nServers; otherProcess++ {
-		port := os.Args[otherProcess + nonOtherServers]
-
-		ServerAddr, err := net.ResolveUDPAddr("udp", port)
-		CheckError(err)
-
-		LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
-		CheckError(err)
-
-		CliConn[otherProcess], err = net.DialUDP("udp", LocalAddr, ServerAddr)
-		CheckError(err)
-	}
-
-	// init server
-	ServerAddr, err := net.ResolveUDPAddr("udp", myPort)
+	ServerAddr, err := net.ResolveUDPAddr("udp", ":10001")
 	CheckError(err)
 
 	ServerConn, err = net.ListenUDP("udp", ServerAddr)
@@ -124,7 +61,9 @@ func initConnections() error {
 }
 
 func readInput(ch chan string) {
-	// Non-blocking async routine to listen for terminal input
+	/*
+		Non-blocking async routine to listen for terminal input
+	 */
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		text, _, _ := reader.ReadLine()
@@ -142,10 +81,6 @@ func main() {
 
 	defer ServerConn.Close()
 
-	for i := 0; i < nServers; i++ {
-		defer CliConn[i].Close()
-	}
-
 	go readInput(ch)
 
 	for {
@@ -154,16 +89,6 @@ func main() {
 			case x, valid := <-ch:
 				if valid {
 					fmt.Printf("Destiny Id: %s \n", x)
-					destiny, err := strconv.Atoi(x)
-					CheckError(err)
-
-					destiny--
-					if destiny >= nServers {
-						err = errors.New("Id out of range")
-						PrintError(err)
-					} else {
-						go doClientJob(destiny)
-					}
 				} else {
 					fmt.Println("Channel Closed!")
 				}
